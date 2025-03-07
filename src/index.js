af79beacab1559b4830e977cac2e740cb80e8a7c
Adrien Rouchet <adrien.rouchet@pm.me>
2025-03-07 11:34:21 +0100

    Update index.html and index.js

--- src/index.js
+++ src/index.js
@@ -6,25 +6,38 @@ When you've set that up, invoke the function to see if it works.
 */
 
 const theCoders = (driver, navigator) => {
-    console.log(
-      `The awesome coders for today is the driver ${driver} and the navigator ${navigator}`
-    );
-  };
-  theCoders("jennie", "tom");
-  /*
+  console.log(
+    `The awesome coders for today is the driver ${driver} and the navigator ${navigator}`
+  );
+};
+theCoders('jennie', 'charlie');
+
+/*
   2)
   Pass in only ONE name when you call the function and see what happens.
   (HINT, you should get undefined...)
   */
-  
-  /*
+
+/*
   3) 
   create a function that checks which name is the longest when comparing the coders 
   and print out: "The driver has the longest name." or "The navigator has the longest name" 
   or if the names ar the same length "The driver and the navigator has equally long names"
   */
-  
-  /*
+
+const checkNameLength = (driver, navigator) => {
+  if (driver.length > navigator.length) {
+    console.log(`${driver} has the longest name.`);
+  } else if (navigator.length > driver.length) {
+    console.log(`${navigator} has the longest name.`);
+  } else {
+    console.log(`${driver} and the ${navigator} has equally long names`);
+  }
+};
+
+checkNameLength('Christopher', 'Adrien');
+
+/*
   4) 
   to get user input you can use the prompt() and then store that into a variable.
   Such as 
@@ -34,9 +47,17 @@ const theCoders = (driver, navigator) => {
   or alert('23! Younger than Zlatan') or something like that.
   Add as many conditonals as you want.
   */
-  
-  const calculationTest = () => {
-    /*
+
+const askUserAge = () => {
+  let userAge = prompt(`How old are you?`);
+  if (userAge > 0) {
+    alert('Congratulation, you are alive');
+  }
+};
+
+askUserAge();
+
+/*
     5)
     In this function we want to give the user a mathematical calculation to 
     give us the answer to. Something like prompt('What is 5 + 6?')  
@@ -44,11 +65,25 @@ const theCoders = (driver, navigator) => {
     If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
     You choose if to use addition, subtraction, multiplication or division. 
     */
-  };
-  
-  /*
+
+const calculationTest = () => {
+  const a = Math.floor(Math.random() * 11);
+  const b = Math.floor(Math.random() * 11);
+  result = a + b;
+  console.log(result);
+  let answer = Number(prompt(`What is ${a} + ${b}?`));
+
+  alert(
+    answer === result
+      ? `Congratulations! You are correct the result is: ${result}`
+      : `Noo! Sorry ${a} + ${b} is not equal to ${answer} but is equal to ${result}`
+  );
+};
+
+calculationTest();
+
+/*
   6) **BONUS**
   Make the calculation machine we just made show random calculations everytime you invoke the function.
   But hey, maybe limit the randomness to be numbers between 0-10?
   */
-  
